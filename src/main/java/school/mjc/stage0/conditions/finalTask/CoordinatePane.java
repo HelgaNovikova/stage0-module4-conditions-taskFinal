e942c566721ac619b103fd1e5cbc33317fb543c2
HelgaNovikova <helpotaki@rambler.ru>
2022-10-31 16:44:46 -0400

    task4final

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x == 0 || y == 0) {
+            System.out.println("zero");
+        } else {
+            String positiveX = "";
+            String positiveY = "";
+            if (x > 0) {
+                positiveX = "1";
+            } else positiveX = "0";
+            if (y > 0) {
+                positiveY = "1";
+            } else positiveY = "0";
+
+            switch (positiveX + positiveY) {
+                case "10":
+                    System.out.println("fourth");
+                    break;
+                case "11":
+                    System.out.println("first");
+                    break;
+                case "01":
+                    System.out.println("second");
+                    break;
+                case "00":
+                    System.out.println("third");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,51 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (month == 2) {
+            if (isLeapYear(year) == true) {
+                System.out.println(29);
+            } else System.out.println(28);
+        } else {
+            amountOfDays(month);
+        }
 
+    }
+
+    public boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 > 0) {
+                return true;
+            } else if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
+
+    public void amountOfDays(int month) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            default:
+                System.out.println("invalid date");
+
+        }
     }
 }
